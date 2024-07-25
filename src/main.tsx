077c9760cc281cb46bb12e88fa4ef2a5a0d94d7f
guanjunjiang <934750771@qq.com>
2024-07-25 13:41:22 +0800

    引入样式

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <FluentProvider theme={webLightTheme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </FluentProvider>
+);
