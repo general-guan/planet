aa18199f1476d3072189502cbba6adbc1d3ab453
guanjunjiang <934750771@qq.com>
2024-07-25 15:27:17 +0800

    删除 fluentui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import "./App.css";
-import { Button } from "@fluentui/react-components";
 import { useCountDown } from "./hooks/useCountDown";
 
 function App() {
   const { label, time, startFn } = useCountDown();
   return (
     <>
-      <Button onClick={startFn}>开始</Button>
+      <button onClick={startFn}>开始</button>
       <div>{label}</div>
       <div>{time}</div>
     </>
